37c26610703b4582e8302364a3b2d48b4277e8ac
Josh Hill <85208628+RareChip@users.noreply.github.com>
2025-08-23 18:34:44 -0400

    File Reorganization

--- Goap/GoapSensor.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace RareChip.Goap
-{
-    public abstract class GoapSensor : MonoBehaviour
-    {
-        public abstract void InitializeConditions(GoapAgent agent);
-    }
-}
